2757eff3c51461429d59ef541c8155fad77eff4d
Liam O'Neill <l.oneill20@nuigalway.ie>
2017-11-10 19:54:35 +0000

    Now matches updated assignment instructions.

--- assignment.pl
+++ assignment.pl
@@ -3,18 +3,19 @@ isElementInList(El, [El|_]).
 isElementInList(El, [_|Tail]) :-
     isElementInList(El, Tail).
 
-%Definitions for reverseList(List, ReversedList)
-reverseList([], []).
-reverseList([SingleElement|[]], [SingleElement|[]]).
-reverseList([Head|Tail], ReversedList) :-
-    reverseList(Tail, ReversedTail),
-    ReversedList = [ReversedTail, Head].
-
-%insertElementIntoListEnd(El, List, NewList)
-insertElementIntoListEnd([], List, List).
-
 %Definitions for mergeLists(List1, List2, Merged)
 mergeLists([], List, List).
 mergeLists(List, [], List).
 mergeLists(List1, [SingleElement|[]], [List1, SingleElement]).
 mergeLists(List1, List2, [List1, List2]).
+
+%Definitions for reverseList(List, ReversedList)
+reverseList([], []).
+reverseList([SingleElement|[]], [SingleElement|[]]).
+reverseList([Head|Tail], ReversedList) :-
+    reverseList(Tail, ReversedTail),
+    mergeLists(ReversedTail, Head, ReversedList).
+
+%insertElementIntoListEnd(El, List, NewList)
+insertElementIntoListEnd(El, List, NewList) :-
+    mergeLists(List, [El], NewList).
