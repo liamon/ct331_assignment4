446dbc2a7fb52c7b308b3213995ce0f70134ca5a
Liam O'Neill <l.oneill20@nuigalway.ie>
2017-11-14 11:22:01 +0000

    Some progress on logic of mergeLists - more work needed.

--- assignment.pl
+++ assignment.pl
@@ -4,10 +4,12 @@ isElementInList(El, [_|Tail]) :-
     isElementInList(El, Tail).
 
 %Definitions for mergeLists(List1, List2, Merged)
-mergeLists([], List, List).
+%mergeLists([], List, List).
 mergeLists(List, [], List).
-mergeLists(List1, [SingleElement|[]], [List1, SingleElement]).
-mergeLists(List1, List2, [List1, List2]).
+%mergeLists(List1, [SingleElement|[]], [List1, SingleElement]).
+%mergeLists(List1, List2, [List1, List2]).
+mergeLists(List1, [Head|Tail], Merged) :-
+    mergeLists([List1, Head], Tail, Merged).
 
 %Definitions for reverseList(List, ReversedList)
 reverseList([], []).
